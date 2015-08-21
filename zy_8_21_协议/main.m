ceea11c99857ee9491dec25013e84391c81d71a1
Tianqi Zhang <1120404010@qq.com>
2015-08-21 18:52:39 +0800

    Initial Commit

---
+++ zy_8_21_协议/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  zy_8_21_协议
+//
+//  Created by 张天琦 on 15/8/21.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
